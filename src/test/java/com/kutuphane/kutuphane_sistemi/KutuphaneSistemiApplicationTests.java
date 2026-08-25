3089370a85d8b28d7a4b003811ebc25414440321
Aylin Kucuk <aylinkucuk0805@gmail.com>
2026-08-25 14:13:45 +0300

    İlk commit: Koşu projesi

---
+++ src/test/java/com/kutuphane/kutuphane_sistemi/KutuphaneSistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutuphane.kutuphane_sistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KutuphaneSistemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
